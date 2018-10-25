510c170992a4736d7bcd0afd45c82a4c48913d6a
TB <9965349+nomoney4me@users.noreply.github.com>
2018-10-24 22:04:27 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+      , HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        resolve: {
+          extensions: ['.js', '.jsx']
+        },
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/template.html'
+    })
+  ]
+}
